0caf942795cd635151a1659500dc83893bc98bbb
gael rubin <concilasplebisll@gmail.com>
2023-06-19 13:50:26 +0200

    add mock boat

--- backend/mock-boat.js
+++ backend/mock-boat.js
@@ -1,8 +1,62 @@
 const boats = [
     {
         id: 1,
-        name: "Titanic",
-        description: "lorem ipsum",
+        name: "Barbe Noire",
+        description: "Barbe-Noire : Un pédalo robuste avec une coque en polyéthylène résistant, une capacité de deux passagers, des pédales réglables pour une utilisation confortable et une hélice à entraînement direct pour une propulsion efficace. ipsum",
+        picture: "path of file picture"
+    },
+    {
+        id: 2,
+        name: "Barbe-Rousse",
+        description: "Barbe-Rousse : Un pédalo spacieux en aluminium avec une coque légère, une capacité de quatre passagers, des pédales ergonomiques pour une meilleure adhérence et une direction précise, et des repose-pieds réglables pour un confort optimal. ipsum",
+        picture: "path of file picture"
+    },
+    {
+        id: 3,
+        name: "Anne Bonny",
+        description: "Anne Bonny : Un pédalo compact avec une coque en fibres de verre durable, une capacité de deux passagers, des sièges rembourrés pour un confort supplémentaire, des porte-gobelets pratiques et un système de gouvernail pour une manœuvrabilité aisée.",
+        picture: "path of file picture"
+    },
+    {
+        id: 4,
+        name: "François l'Olonnais",
+        description: "François l'Olonnais : Un pédalo de haute performance avec une coque en aluminium aérodynamique, une capacité de trois passagers, des pédales réglables avec un système de transmission à chaîne pour une propulsion efficace et des stabilisateurs intégrés pour une stabilité accrue.",
+        picture: "path of file picture"
+    },
+    {
+        id: 5,
+        name: "William Kidd",
+        description: "William Kidd : Un pédalo polyvalent avec une coque en plastique renforcé de fibres, une capacité de quatre passagers, un design modulaire pour faciliter le transport et le stockage, ainsi qu'un compartiment de rangement étanche pour garder vos effets personnels au sec.",
+        picture: "path of file picture"
+    },
+    {
+        id: 6,
+        name: "Jean Lafitte",
+        description: "Jean Lafitte : Un pédalo de luxe avec une coque en bois exotique, une capacité de deux passagers, des sièges rembourrés en cuir véritable, des pédales en acier inoxydable et une finition haut de gamme pour une expérience de navigation élégante.",
+        picture: "path of file picture"
+    },
+    {
+        id: 7,
+        name: "Francis Drake",
+        description: "Francis Drake : Un pédalo polyvalent avec une coque en polypropylène résistant aux chocs, une capacité de trois passagers, un système de pédalage fluide avec une transmission par courroie, ainsi qu'un porte-bagages arrière pour transporter des provisions ou des équipements supplémentaires.",
+        picture: "path of file picture"
+    },
+    {
+        id: 8,
+        name: "Mary Read",
+        description: "Mary Read : Un pédalo familial avec une coque en plastique renforcé, une capacité de cinq passagers, des pédales réglables pour s'adapter à différents gabarits, des repose-pieds antidérapants, ainsi qu'une échelle de bain arrière pour faciliter les baignades.",
+        picture: "path of file picture"
+    },
+    {
+        id: 9,
+        name: "Henry Morgan",
+        description: "Henry Morgan : Un pédalo de loisir avec une coque en PVC résistant, une capacité de deux passagers, des sièges inclinables pour une détente optimale, un pare-soleil amovible pour se protéger du soleil et un porte-bouteille intégré pour garder vos boissons à portée de main.",
+        picture: "path of file picture"
+    },
+    {
+        id: 10,
+        name: "Calico Jack Rackham",
+        description: "Calico Jack : Un pédalo polyvalent et léger avec une coque en composite de fibre de verre, une capacité de trois passagers, un système de direction réactif, des pédales antidérapantes et un gouvernail réglable pour une maniabilité précise.",
         picture: "path of file picture"
     },
 ];
