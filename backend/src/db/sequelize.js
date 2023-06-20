7e9a6c5210155a10fbcecef53a2a1001aa194dff
gael rubin <concilasplebisll@gmail.com>
2023-06-20 10:27:25 +0200

    modif sequelize.js

--- backend/src/db/sequelize.js
+++ backend/src/db/sequelize.js
@@ -19,81 +19,55 @@ const User = UserModel(sequelize, DataTypes);
 
 const initDb = () => {
     return new Promise((resolve, reject) => {
-      const maxRetries = 10; // Nombre maximal de tentatives de connexion
-      const retryInterval = 3000; // Intervalle entre chaque tentative de connexion (en millisecondes)
-      let retries = 0;
-  
-      const connect = () => {
-        sequelize
-          .sync({ force: true })
-          .then(() => {
-            console.log('La SYNC à la base de données a réussi.');
-  
-            boats.forEach((boat) => {
-              Boat.create({
-                name: boat.name,
-                description: boat.description,
-                picture: boat.picture,
-              })
-                .then((boat) => console.log(boat.toJSON()))
-                .catch((error) => console.error(`Erreur lors de la création du bateau : ${error}`));
-            });
-  
-            bcrypt.hash('1234', 10)
-              .then((hash) => {
-                User.create({
-                  username: 'gael',
-                  password: hash,
+        const maxRetries = 10; // Nombre maximal de tentatives de connexion
+        const retryInterval = 3000; // Intervalle entre chaque tentative de connexion (en millisecondes)
+        let retries = 0;
+
+        const connect = () => {//fonction pour éviter un tentative de connection trop rapide de l'app sur une bbd pas encore up
+            sequelize
+                .sync({ force: true })//enlever force: true par la suite
+                .then(() => {
+                console.log('La SYNC à la base de données a réussi.');
+
+                boats.forEach((boat) => {
+                    Boat.create({
+                    name: boat.name,
+                    description: boat.description,
+                    picture: boat.picture,
+                    })
+                    .then((boat) => console.log(boat.toJSON()))
+                    .catch((error) => console.error(`Erreur lors de la création du bateau : ${error}`));
+                });
+
+                bcrypt.hash('1234', 10)
+                    .then((hash) => {
+                    User.create({
+                        username: 'gael',
+                        password: hash,
+                    })
+                        .then((user) => console.log(user.toJSON()))
+                        .catch((error) => console.error(`Erreur lors de la création de l'utilisateur : ${error}`));
+                    })
+                    .catch((error) => console.error(`Erreur lors du hachage du mot de passe : ${error}`));
+
+                resolve();// est utilisée pour déclencher le succès d'une promesse
                 })
-                  .then((user) => console.log(user.toJSON()))
-                  .catch((error) => console.error(`Erreur lors de la création de l'utilisateur : ${error}`));
-              })
-              .catch((error) => console.error(`Erreur lors du hachage du mot de passe : ${error}`));
-  
-            resolve();
-          })
-          .catch((error) => {
-            if (retries < maxRetries) {
-              retries++;
-              console.log(`Tentative de connexion à la base de données (${retries}/${maxRetries})...`);
-              setTimeout(connect, retryInterval);
-            } else {
-              reject(`Impossible de se connecter à la base de données : ${error}`);
-            }
-          });
-      };
-  
-      console.log('Initialisation de la base de données...');
-      connect();
+                .catch((error) => {
+                if (retries < maxRetries) {
+                    retries++;
+                    console.log(`Tentative de connexion à la base de données (${retries}/${maxRetries})...`);
+                    setTimeout(connect, retryInterval);
+                } else {
+                    reject(`Impossible de se connecter à la base de données : ${error}`);// est utilisée pour déclencher un échec.
+                }
+            });
+        };
+
+        console.log('Initialisation de la base de données...');
+        connect();
     });
-  };
+};
   
-/*
-const initDb = () => {
-    return sequelize.sync({force: true}).then(_ => {
-        boats.map(boat => {
-            Boat.create({
-                name: boat.name,
-                description: boat.description,
-                picture: boat.picture
-            })
-            .then(boat => console.log(boat.toJSON()));
-        })
-
-        bcrypt.hash('1234', 10)
-        .then(hash => {
-            User.create({
-                username: 'gael',
-                password: hash
-            })
-        })
-        .then(user => console.log(user.toJSON()))
-
-        console.log('La SYNC à la bdd a réussi.')
-    })
-    .catch(error => console.error(`impossible de SYNC a la bdd : ${error}`))
-}
-*/
 module.exports = {
     initDb, Boat, User, sequelize
 }
